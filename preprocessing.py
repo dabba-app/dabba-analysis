74e0dda810b39ce8161e9a65e8e13f5d0662d829
Piyush.ku <piyush.ku@BMMDVPIYUSHKU.local>
2018-03-17 18:50:04 +0530

    image processing

--- preprocessing.py
+++ preprocessing.py
@@ -1,5 +1,10 @@
 import cv2
 
+
+#image 1 -> the image of the dustbin before throwing in the new garbage(object of interest)
+#image2 -> the image of the dustbin after throwing in the new garbage(object of interest)
+#plz click images from a steady camera, else the algo wont work
+
 image1 = cv2.imread("/Users/piyush.ku/Desktop/im1.png")
 #image1 = cv2.resize(image1,None,fx=0.1, fy=0.1, interpolation = cv2.INTER_CUBIC)
 image1 = cv2.cvtColor(image1, cv2.COLOR_BGR2GRAY)
@@ -21,7 +26,6 @@ cv2.imwrite('/Users/piyush.ku/Desktop/threshImage.png',threshImage)
 clrimg = cv2.imread("/Users/piyush.ku/Desktop/im1.png")
 abc = cv2.bitwise_and(clrimg, clrimg, mask = cv2.imread('/Users/piyush.ku/Desktop/threshImage.png', 0))
 #cv2.Copy(clrimg, clrimg, threshImage)
-
 
 cv2.imshow('Draw05',abc)
 
