58648ea5dbf4576b50ab0419346c640ba15eba14
Piyush.ku <piyush.ku@BMMDVPIYUSHKU.local>
2018-03-28 01:46:44 +0530

    refactor

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import requests
-#from requests.auth import HTTPDigestAuth
 import json
 from pprint import pprint
 import kmeans as km
@@ -10,33 +9,26 @@ import send_asynch_message as sam
 # Replace with the correct URL
 url = "http://dabba.us-west-2.elasticbeanstalk.com/bins/"
 
-# It is a good practice not to hardcode the credentials. So ask the user to enter credentials at runtime
-#myResponse = requests.get(url,auth=HTTPDigestAuth(raw_input("username: "), raw_input("Password: ")), verify=True)
-
 myResponse = requests.get(url)
 print (myResponse.status_code)
 #print(myResponse.content)
 
-# For successful API call, response code will be 200 (OK)
 if(myResponse.ok):
     data = json.loads(myResponse.content,encoding="utf-8")
     listOfFilledDustbins = dict()
     listOfFilledDustbins['V1'] = list()
     listOfFilledDustbins['V2'] = list()
     for i in range(len(data)):
-        #pprint(data[i])
         if(data[i]['LEVEL'] > 10):
             listOfFilledDustbins['V1'].append(float(data[i]['LAT']))
             listOfFilledDustbins['V2'].append(float(data[i]['LONG']))
 
-    #print(listOfFilledDustbins)
     clusters, C, X = km.kmeans(listOfFilledDustbins)
     for i in range(len(C)):
         points = np.array([X[j] for j in range(len(X)) if clusters[j] == i])
         link = tsp.runTsp(points)
         sam.send_message("piyush9620", link + " Follow this route to collect garbage.")
         #print(points)
-
 
 else:
   # If response code is not ok (200), print the resulting http error code with description
--- kmeans.py
+++ kmeans.py
@@ -6,21 +6,14 @@ plt.rcParams['figure.figsize'] = (16, 9)
 plt.style.use('ggplot')
 
 def kmeans(data):
-
-    # Importing the dataset
-    #data = pd.read_csv('xclara.csv')
     print("Input Data and Shape")
     #print(data.shape)
     print data
 
-    # Getting the values and plotting it
     f1 = np.array(data['V1'])*1000000
     f2 = np.array(data['V2'])*1000000
     X = np.array(list(zip(f1, f2)))
 
-    #plt.scatter(f1, f2, c='black', s=7)
-
-    # Euclidean Distance Caculator
     def dist(a, b, ax=1):
         return np.linalg.norm(a - b, axis=ax)
 
@@ -28,31 +21,20 @@ def kmeans(data):
     k = int(np.ceil(len(X) / 10.0))
 
     print(k)
-    # X coordinates of random centroids
     C_x = np.random.randint(np.min(X[:,0]), np.max(X[:,0]), size=k)
-    # Y coordinates of random centroids
     C_y = np.random.randint(np.min(X[:,1]), np.max(X[:,1]), size=k)
 
     C = np.array(list(zip(C_x, C_y)), dtype=np.float32)
     print("Initial Centroids")
     print(C)
 
-    # Plotting along with the Centroids
-    # plt.scatter(f1, f2, c='#050505', s=7)
-    # plt.scatter(C_x, C_y, marker='*', s=200, c='g')
-
-
     # To store the value of centroids when it updates
     C_old = np.zeros(C.shape)
     # Cluster Lables(0, 1, 2)
     clusters = np.zeros(len(X))
-    print(C.shape)
-    print(len(X))
-    print(X.__class__)
-    print(X.shape)
     # Error func. - Distance between new centroids and old centroids
     error = dist(C, C_old, None)
-    # Loop will run till the error becomes zero
+
     while error != 0:
         # Assigning each value to its closest cluster
         for i in range(len(X)):
@@ -75,7 +57,6 @@ def kmeans(data):
     fig, ax = plt.subplots()
     for i in range(k):
             points = np.array([X[j] for j in range(len(X)) if clusters[j] == i])
-            #print(points)
             if len(points) != 0:
                 ax.scatter(points[:, 0], points[:, 1], s=7, c=colors[i])
     ax.scatter(C[:, 0], C[:, 1], marker='*', s=200, c='#050505')
