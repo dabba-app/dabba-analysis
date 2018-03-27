1de850254e4e9bf80ec6a8a2b3fdba63c1d1fb92
Piyush.ku <piyush.ku@bmmdvpiyushku.internal.directi.com>
2018-03-27 13:58:36 +0530

    updated

---
+++ app.py
@@ -0,0 +1,40 @@
+import requests
+#from requests.auth import HTTPDigestAuth
+import json
+from pprint import pprint
+import kmeans as km
+import numpy as np
+import tsp
+
+# Replace with the correct URL
+url = "http://dabba.us-west-2.elasticbeanstalk.com/bins/"
+
+# It is a good practice not to hardcode the credentials. So ask the user to enter credentials at runtime
+#myResponse = requests.get(url,auth=HTTPDigestAuth(raw_input("username: "), raw_input("Password: ")), verify=True)
+
+myResponse = requests.get(url)
+print (myResponse.status_code)
+#print(myResponse.content)
+
+# For successful API call, response code will be 200 (OK)
+if(myResponse.ok):
+    data = json.loads(myResponse.content,encoding="utf-8")
+    listOfFilledDustbins = dict()
+    listOfFilledDustbins['V1'] = list()
+    listOfFilledDustbins['V2'] = list()
+    for i in range(len(data)):
+        #pprint(data[i])
+        if(data[i]['LEVEL'] > 10):
+            listOfFilledDustbins['V1'].append(float(data[i]['LAT']))
+            listOfFilledDustbins['V2'].append(float(data[i]['LONG']))
+
+    #print(listOfFilledDustbins)
+    clusters, C, X = km.kmeans(listOfFilledDustbins)
+    for i in range(len(C)):
+        points = np.array([X[j] for j in range(len(X)) if clusters[j] == i])
+        tsp.runTsp(points)
+        #print(points)
+
+else:
+  # If response code is not ok (200), print the resulting http error code with description
+    myResponse.raise_for_status()
--- kmeans.py
+++ kmeans.py
@@ -5,64 +5,79 @@ from matplotlib import pyplot as plt
 plt.rcParams['figure.figsize'] = (16, 9)
 plt.style.use('ggplot')
 
-# Importing the dataset
-data = pd.read_csv('xclara.csv')
-#print("Input Data and Shape")
-#print(data.shape)
-#data.head
+def kmeans(data):
 
-# Getting the values and plotting it
-f1 = data['V1'].values
-f2 = data['V2'].values
-X = np.array(list(zip(f1, f2)))
-plt.scatter(f1, f2, c='black', s=7)
+    # Importing the dataset
+    #data = pd.read_csv('xclara.csv')
+    print("Input Data and Shape")
+    #print(data.shape)
+    print data
 
-# Euclidean Distance Caculator
-def dist(a, b, ax=1):
-    return np.linalg.norm(a - b, axis=ax)
+    # Getting the values and plotting it
+    f1 = np.array(data['V1'])*1000000
+    f2 = np.array(data['V2'])*1000000
+    X = np.array(list(zip(f1, f2)))
 
-# Number of clusters
-k = 3
-# X coordinates of random centroids
-C_x = np.random.randint(0, np.max(X)-20, size=k)
-# Y coordinates of random centroids
-C_y = np.random.randint(0, np.max(X)-20, size=k)
+    #plt.scatter(f1, f2, c='black', s=7)
 
-C = np.array(list(zip(C_x, C_y)), dtype=np.float32)
-print("Initial Centroids")
-print(C)
+    # Euclidean Distance Caculator
+    def dist(a, b, ax=1):
+        return np.linalg.norm(a - b, axis=ax)
 
-# Plotting along with the Centroids
-plt.scatter(f1, f2, c='#050505', s=7)
-plt.scatter(C_x, C_y, marker='*', s=200, c='g')
+    # Number of clusters = no of dustbins / 50; max clusters = number of trucks
+    k = int(np.ceil(len(X) / 10.0))
 
-# To store the value of centroids when it updates
-C_old = np.zeros(C.shape)
-# Cluster Lables(0, 1, 2)
-clusters = np.zeros(len(X))
-# Error func. - Distance between new centroids and old centroids
-error = dist(C, C_old, None)
-# Loop will run till the error becomes zero
-while error != 0:
-    # Assigning each value to its closest cluster
-    for i in range(len(X)):
-        distances = dist(X[i], C)
-        #returns index of the min element
-        cluster = np.argmin(distances)
-        clusters[i] = cluster
-    # Storing the old centroid values
-    C_old = deepcopy(C)
-    # Finding the new centroids by taking the average value
-    for i in range(k):
-        points = [X[j] for j in range(len(X)) if clusters[j] == i]
-        C[i] = np.mean(points, axis=0)
+    print(k)
+    # X coordinates of random centroids
+    C_x = np.random.randint(np.min(X[:,0]), np.max(X[:,0]), size=k)
+    # Y coordinates of random centroids
+    C_y = np.random.randint(np.min(X[:,1]), np.max(X[:,1]), size=k)
+
+    C = np.array(list(zip(C_x, C_y)), dtype=np.float32)
+    print("Initial Centroids")
+    print(C)
+
+    # Plotting along with the Centroids
+    # plt.scatter(f1, f2, c='#050505', s=7)
+    # plt.scatter(C_x, C_y, marker='*', s=200, c='g')
+
+
+    # To store the value of centroids when it updates
+    C_old = np.zeros(C.shape)
+    # Cluster Lables(0, 1, 2)
+    clusters = np.zeros(len(X))
+    print(C.shape)
+    print(len(X))
+    print(X.__class__)
+    print(X.shape)
+    # Error func. - Distance between new centroids and old centroids
     error = dist(C, C_old, None)
+    # Loop will run till the error becomes zero
+    while error != 0:
+        # Assigning each value to its closest cluster
+        for i in range(len(X)):
+            distances = dist(X[i], C)
+            #returns index of the min element
+            cluster = np.argmin(distances)
+            clusters[i] = cluster
+        # Storing the old centroid values
+        C_old = deepcopy(C)
+        # Finding the new centroids by taking the average value
+        for i in range(k):
+            points = [X[j] for j in range(len(X)) if clusters[j] == i]
+            if len(points) != 0:
+                C[i] = np.mean(points, axis=0)
+        error = dist(C, C_old, None)
 
-
-colors = ['r', 'g', 'b', 'y', 'c', 'm']
-fig, ax = plt.subplots()
-for i in range(k):
-        points = np.array([X[j] for j in range(len(X)) if clusters[j] == i])
-        ax.scatter(points[:, 0], points[:, 1], s=7, c=colors[i])
-ax.scatter(C[:, 0], C[:, 1], marker='*', s=200, c='#050505')
-plt.show()
+    X = X / 1000000;
+    C = C / 1000000;
+    colors = ['r', 'g', 'b', 'y', 'c', 'm']
+    fig, ax = plt.subplots()
+    for i in range(k):
+            points = np.array([X[j] for j in range(len(X)) if clusters[j] == i])
+            #print(points)
+            if len(points) != 0:
+                ax.scatter(points[:, 0], points[:, 1], s=7, c=colors[i])
+    ax.scatter(C[:, 0], C[:, 1], marker='*', s=200, c='#050505')
+    plt.show()
+    return clusters, C, X
